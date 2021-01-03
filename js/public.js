15b61c9f576a9acf7ee88cfad165eaa2add628a5
zhengfan2014 <zhengfan2014@outlook.com>
2021-01-03 23:41:04 +0800

    update

--- js/public.js
+++ js/public.js
@@ -1,4 +1,5 @@
 const JSONList = [
+    { id: "202004", name: '2018级Python 期末选择题20道', describe: '2018级Python 期末选择题20道', file: "python2018.json" },
     { id: "202003", name: '2018级企业网络构建 期末选择题100道', describe: '2018级学生 期末练习 选择题100道', file: "qiye2018.json" },
 	{ id: "202001", name: '2019级企业网络构建 期末选择题70道', describe: '2019级学生 期末练习 选择题70道', file: "qiye2019.json" },
     { id: "201610", name: '00896 电子商务概论 2016年10月题库', describe: '00896 电子商务概论 201610真题试卷，包含选择和简答题，不包含分析和材料题', file: "2016 10.json" },
